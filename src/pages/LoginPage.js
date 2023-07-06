b1ca5cff6e5490df87e6710ef4a7e57554b100a7
snehangshu <snehangshu1228@gmail.com>
2023-07-07 03:22:01 +0530

    designe loginpage and sineuppage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../HomePage.css';
+import loginbackgroundimg from "../images/backgroundimg.avif";
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 export default function LoginPage() {
+
+
   return (
-    <div>LoginPage</div>
+    <div className='login-back'>
+
+      <div className='inner-box'>
+      <h1 className='heading'>Login</h1>
+            <input className='log-inputBox' type='text'  placeholder='enter your email'/>
+            <input className='log-inputBox' type='password'   placeholder='enter your password'/>
+          <div className='btn'>
+            <button>log-in</button>
+          </div>
+          <hr></hr>
+          <p className='p-or'>or</p>
+
+          <div className='browser-login'>
+            <idv><GoogleIcon/></idv>
+            <idv><FacebookIcon/></idv>
+            <idv><TwitterIcon/></idv>
+          </div>
+          <p>
+              Don't have an Account?{''}
+              <span >
+                <Link to="/singup" className='link-singup'>SingUp</Link>
+              </span>
+            </p>
+      </div>
+    </div>
   )
 }
--- src/pages/SingUpPage.js
+++ src/pages/SingUpPage.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../HomePage.css';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function SingUpPage() {
   return (
-    <div>SingUpPage</div>
+    <div className='login-back'>
+      <div className='inner-box'>
+      <h1 className='heading'>SingUp</h1>
+            <input className='log-inputBox' type='text'  placeholder='userName'/>
+            <input className='log-inputBox' type='text'  placeholder='enter your email'/>
+            <input className='log-inputBox' type='password'   placeholder='enter your password'/>
+          <div className='btn'>
+            <button>Sing-up</button>
+          </div>
+          <hr></hr>
+          <p className='p-or'>or</p>
+
+          <div className='browser-login'>
+            <idv><GoogleIcon/></idv>
+            <idv><FacebookIcon/></idv>
+            <idv><TwitterIcon/></idv>
+          </div>
+          <p>
+              Already have an Account?{''}
+              <span >
+                <Link to="/login" className='link-singup'>Login</Link>
+              </span>
+            </p>
+      </div>
+    </div>
   )
 }
